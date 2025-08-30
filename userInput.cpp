23e9f52ad35f747aa1a47bb3c9fd3e2169e03fb2
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 08:32:13 +0600

    add 1st example of 'User-Input' in C++

---
+++ userInput.cpp
@@ -0,0 +1,20 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic: User Input
+// ================================================
+
+// ===== 👉👉 🔹🔹 example  : 1️⃣
+
+#include <iostream>
+using namespace std;
+int main(){
+ int x, y;
+ cout <<"Enter Number";
+ cin>>x ;
+ cout <<"You Entered " << x << endl;
+
+    return 0;
+}
+
+// 👉👉  🔹🔹  Example
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
