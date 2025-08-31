fdfaa67c709266837ff91713313c943a5f458a2d
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-31 23:34:06 +0600

    add example of tyepCast in C++

--- tyepCasting.cpp
+++ tyepCasting.cpp
@@ -1,10 +1,24 @@
+// =================== Example ===================
 #include <iostream>
 using namespace std;
 int main(){
  int x ;
  cout<<"Enter Number  : " ;
  cin>>  x;
- float y = (float )x 
- cout<<"output is : " << x/2<<endl;
+ float y = (float )x ;
+ cout<<"output is : " << y<<endl;
+    return 0;
+}
+
+
+// =================== Example ===================
+#include <iostream>
+using namespace std;
+int main(){
+ int x ;
+ cout<<"Enter Number  : " ;
+ cin>>  x;
+ float y = (float )x ;
+ cout<<"output is : " << y/2<<endl;
     return 0;
 }
