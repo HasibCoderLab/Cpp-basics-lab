220d1498d2356b1a58d7817df69622e557fca543
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-31 08:05:24 +0600

    update  file

--- modulusOperator.cpp
+++ modulusOperator.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+    int x = 10 ;
+    int y = 3;
+    cout<< x %y;
+    cout<<endl;
     return 0;
 }
---
+++ tyepCasting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int x ;
+ cout<<"Enter Number  : " ;
+ cin>>  x;
+ float y = (float )x 
+ cout<<"output is : " << x/2<<endl;
+    return 0;
+}
